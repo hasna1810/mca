6d03a22cccdc646ea34324bfc2cb0dd1b1c238eb
hasna1810 <121877642+hasna1810@users.noreply.github.com>
2024-12-09 19:34:42 +0530

    disjoint.c

--- disjoint.c
+++ disjoint.c
@@ -1,126 +1,97 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-#define MAX 100
+int i, setA[10], setB[10], setC[20], m, n;
+void create();
+void unionset();
+void is_disjoint();
 
-typedef struct {
-    int elements[MAX];
-    int size;
-} Set;
-
-// Function to create a set
-void createSet(Set *set, int *arr, int size) {
-    set->size = size;
-    for (int i = 0; i < size; i++) {
-        set->elements[i] = arr[i];
-    }
-}
-
-// Function to display a set
-void displaySet(Set *set) {
-    printf("{ ");
-    for (int i = 0; i < set->size; i++) {
-        printf("%d ", set->elements[i]);
-    }
-    printf("}\n");
-}
-
-// Function to find if an element exists in a set
-int findInSet(Set *set, int element) {
-    for (int i = 0; i < set->size; i++) {
-        if (set->elements[i] == element) {
-            return 1; // Found
-        }
-    }
-    return 0; // Not Found
-}
-
-// Function to perform union of two sets
-void unionSets(Set *setA, Set *setB, Set *result) {
-    int k = 0;
-
-    // Add all elements of setA
-    for (int i = 0; i < setA->size; i++) {
-        result->elements[k++] = setA->elements[i];
-    }
-
-    // Add elements of setB that are not in setA
-    for (int i = 0; i < setB->size; i++) {
-        if (!findInSet(setA, setB->elements[i])) {
-            result->elements[k++] = setB->elements[i];
-        }
-    }
-
-    result->size = k;
-}
-
-// Function to check if two sets are disjoint
-int areDisjoint(Set *setA, Set *setB) {
-    for (int i = 0; i < setA->size; i++) {
-        if (findInSet(setB, setA->elements[i])) {
-            return 0; // Not disjoint
-        }
-    }
-    return 1; // Disjoint
-}
-
-int main() {
-    Set setA, setB, unionSet;
-    int choice, n, m, arrA[MAX], arrB[MAX];
-
-    printf("Enter the number of elements in Set A: ");
-    scanf("%d", &n);
-    printf("Enter the elements of Set A: ");
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arrA[i]);
-    }
-    createSet(&setA, arrA, n);
-
-    printf("Enter the number of elements in Set B: ");
-    scanf("%d", &m);
-    printf("Enter the elements of Set B: ");
-    for (int i = 0; i < m; i++) {
-        scanf("%d", &arrB[i]);
-    }
-    createSet(&setB, arrB, m);
-
-    do {
-        printf("\nMenu:\n");
-        printf("1. Display Sets\n");
-        printf("2. Union of Sets\n");
-        printf("3. Check if Disjoint\n");
-        printf("4. Exit\n");
+int main()
+{
+    int c;
+    while (1)
+    {
+        printf("\n1. Creation\n2. Union\n3. Check Disjoint\n4. Exit\n");
         printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
+        scanf("%d", &c);
+        switch (c)
+        {
             case 1:
-                printf("Set A: ");
-                displaySet(&setA);
-                printf("Set B: ");
-                displaySet(&setB);
+                create();
                 break;
             case 2:
-                unionSets(&setA, &setB, &unionSet);
-                printf("Union of Set A and Set B: ");
-                displaySet(&unionSet);
+                unionset();
                 break;
             case 3:
-                if (areDisjoint(&setA, &setB)) {
-                    printf("Set A and Set B are disjoint.\n");
-                } else {
-                    printf("Set A and Set B are not disjoint.\n");
-                }
+                is_disjoint();
                 break;
             case 4:
-                printf("Exiting program.\n");
-                break;
+                return 0;
+
             default:
-                printf("Invalid choice! Try again.\n");
+                printf("\nInvalid choice\n");
         }
-    } while (choice != 4);
-
-
+    }
     return 0;
 }
 
+void create()
+{
+    printf("Enter the limit of the first set: ");
+    scanf("%d", &m);
+    printf("Enter the first set:\n");
+    for (i = 1; i <= m; i++)
+        scanf("%d", &setA[i]);
+    printf("Enter the limit of the second set: ");
+    scanf("%d", &n);
+    printf("Enter the second set:\n");
+    for (i = 1; i <= n; i++)
+        scanf("%d", &setB[i]);
+    printf("\nSET A: {");
+    for (i = 1; i <= m; i++)
+        printf("%d,", setA[i]);
+    printf("}\n");
+    printf("SET B: {");
+    for (i = 1; i <= n; i++)
+        printf("%d,", setB[i]);
+    printf("}\n");
+}
+
+void unionset()
+{
+    int j;
+    for (i = 1; i <= m; i++)
+        setC[i] = setA[i];
+    j = m + 1;
+    for (i = 1; i <= n; i++)
+    {
+        setC[j] = setB[i];
+        j++;
+    }
+    printf("\nUNION SET: {");
+    for (i = 1; i <= m + n; i++)
+        printf("%d,", setC[i]);
+    printf("}\n");
+}
+
+void is_disjoint()
+{
+    int isDisjoint = 1; // Flag to check if sets are disjoint
+    for (i = 1; i <= m; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (setA[i] == setB[j])
+            {
+                isDisjoint = 0; // Common element found
+                break;
+            }
+        }
+        if (!isDisjoint)
+            break;
+    }
+
+    if (isDisjoint)
+        printf("\nThe sets are disjoint.\n");
+    else
+        printf("\nThe sets are not disjoint.\n");
+}
